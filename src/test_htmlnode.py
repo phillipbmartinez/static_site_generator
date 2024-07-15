89a922890f87dd2778059af0926de760e9d4d799
Phillip Martinez <phillip.martinez210@gmail.com>
2024-07-15 11:58:28 -0500

    Changes made to refresh project

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -15,6 +15,40 @@ class TestHTMLNode(unittest.TestCase):
             ' class="greeting" href="https://boot.dev"',
         )
 
+    def test_values(self):
+        node = HTMLNode(
+            "div",
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.tag,
+            "div",
+        )
+        self.assertEqual(
+            node.value,
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.children,
+            None,
+        )
+        self.assertEqual(
+            node.props,
+            None,
+        )
+
+    def test_repr(self):
+        node = HTMLNode(
+            "p",
+            "What a strange world",
+            None,
+            {"class": "primary"},
+        )
+        self.assertEqual(
+            node.__repr__(),
+            "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
+        )
+
     def test_to_html_no_children(self):
         node = LeafNode("p", "Hello, world!")
         self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
